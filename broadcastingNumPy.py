25ecdbf8a4d589694fc288456d7faafc6e674d0b
aaronBurns59 <aaronburns59@gmail.com>
2019-09-09 18:09:15 +0100

    Praticing Broadcasting with NumpPy in Python

---
+++ broadcastingNumPy.py
@@ -0,0 +1,24 @@
+from numpy import array
+# Scaler and a 1D Array addition
+a = array([1,2,3])
+print(a)
+b = 2
+print(b)
+c = a + b
+print(c)
+
+# Scaler and a 2D Array addition 
+a = array([[1,2,3],[4,5,6]])
+print(a)
+b=3
+print(b)
+c = a + b
+print(c)
+
+# 1D Array and a 2D Array addition
+a = array([[1,2,3],[4,5,6]])
+print(a)
+b = array([1,2,3])
+print(b)
+c = a + b
+print(c)
